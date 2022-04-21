15c3190b508724eb4c843c74f65219f1da1af3c1
tgilkerson <tgilkerson@miletwo.us>
2022-04-21 00:21:33 -0400

    add stepper example

---
+++ proj02/go.mod
@@ -0,0 +1,5 @@
+module ezstepper
+
+go 1.17
+
+require tinygo.org/x/drivers v0.19.0
--- proj02/main.go
+++ proj02/main.go
@@ -1,6 +1,34 @@
 package main
-import "fmt"
 
-func main() { 
-	fmt.Println("Hello World!")
+import (
+	"machine"
+	"time"
+
+	"tinygo.org/x/drivers/easystepper"
+)
+
+
+func main() {
+	var pin13 machine.Pin = 13 // to IN01 on controler board
+	var pin15 machine.Pin = 15 // to IN02 on controler board
+	var pin14 machine.Pin = 14 // to IN03 on controler board
+	var pin16 machine.Pin = 16 // to IN04 on controler board
+	pin13.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin15.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin14.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin16.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	motor := easystepper.New( pin13, pin15, pin14, pin16,	200, 75	)
+  //	motor := easystepper.New(machine.P13, machine.P15, machine.P14, machine.P16, 200, 75)
+	motor.Configure()
+
+	for {
+		println("CLOCKWISE")
+		motor.Move(2050)
+		time.Sleep(time.Millisecond * 1000)
+
+		println("COUNTERCLOCKWISE")
+		motor.Move(-2050)
+		time.Sleep(time.Millisecond * 1000)
+	}
 }
