e78f36961711dd292416d1d617a067ad5128f37f
tgilkerson <tgilkerson@miletwo.us>
2022-04-24 23:49:11 -0400

    got G25/G36 working on M5

--- proj03/main.go
+++ proj03/main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	println("Hi M5 4")
 
+	var pin25 machine.Pin = 25
+	var pin36 machine.Pin = 36
+	pin25.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin36.Configure(machine.PinConfig{Mode: machine.PinMode(machine.NoPin)})
+
+
 	var pin10LED machine.Pin = 10
 	pin10LED.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
@@ -20,12 +26,14 @@ func main() {
 	for {
 		println("Pin on")
 		pin10LED.High()
+		pin25.High()
 		pin32.High()
 		pin33.High()
 		time.Sleep(time.Second)
 
 		println("Pin off")
 		pin10LED.Low()
+		pin25.Low()
 		pin32.Low()
 		pin33.Low()
 		time.Sleep(time.Second)
