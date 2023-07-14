24d2b92e95644b6d727db4b653c8864dfc0d1783
Tony Gilkerson <tgilkerson@miletwo.us>
2023-07-13 23:13:53 -0400

    do fonts

--- proj29-epaper/main.go
+++ proj29-epaper/main.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"tinygo.org/x/drivers/waveshare-epd/epd4in2"
+	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freemono"
 	// "tinygo.org/x/tinyfont"
 	// "tinygo.org/x/tinyfont/freemono"
 )
@@ -46,41 +48,15 @@ func main() {
 	println("Configure epd4in2")
 	display.Configure(epd4in2.Config{})
 
-	black := color.RGBA{1, 1, 1, 255}
-
-	println("ClearBuffer")
-	display.ClearBuffer()
-	println("ClearDisplay")
-	display.ClearDisplay()
-	println("WaitUntilIdle")
-	display.WaitUntilIdle()
-	println("Waiting for 3 seconds")
-	time.Sleep(3 * time.Second)
-
+	// clearDisplay(&display)
+	// displayCheckerBoard(&display)
 	
-
-	println("Prep checkered board")
-	// Show a checkered board
-	for i := int16(0); i < 16; i++ {
-		for j := int16(0); j < 25; j++ {
-			if (i+j)%2 == 0 {
-				showRect(i*8, j*10, 8, 10, black)
-			}
-		}
-	}
-	
-	println("Waiting for 3 seconds")
-	time.Sleep(3 * time.Second)
-
-	println("Display()")
-	display.Display()
-	println("WaitUntilIdle()")
-	display.WaitUntilIdle()
-
-	println("Waiting for 3 seconds")
+	clearDisplay(&display)
+	fontExamples(&display)
 
 
-
+	println("Waiting for 5 seconds")
+	time.Sleep(5 * time.Second)
 
 	println("You could remove power now")
 }
@@ -91,4 +67,72 @@ func showRect(x int16, y int16, w int16, h int16, c color.RGBA) {
 			display.SetPixel(i, j, c)
 		}
 	}
+}
+
+func clearDisplay(display *epd4in2.Device){
+
+	println("ClearBuffer")
+	display.ClearBuffer()
+	println("ClearDisplay")
+	display.ClearDisplay()
+	println("WaitUntilIdle")
+	display.WaitUntilIdle()
+	println("Waiting for 3 seconds")
+	time.Sleep(3 * time.Second)
+}
+
+func displayCheckerBoard(display *epd4in2.Device){
+	black := color.RGBA{1, 1, 1, 255}
+
+	println("Prep checkered board")
+	// Show a checkered board
+	for i := int16(0); i < 49; i++ {
+		for j := int16(0); j < 29; j++ {
+			if (i+j)%2 == 0 {
+				showRect(i*8, j*10, 8, 10, black)
+			}
+		}
+	}
+
+	println("Waiting for 3 seconds")
+	time.Sleep(3 * time.Second)
+	
+	println("Display()")
+	display.Display()
+	println("WaitUntilIdle()")
+	display.WaitUntilIdle()
+
+	println("Waiting for 3 seconds")
+	time.Sleep(3 * time.Second)
+}
+
+func fontExamples(display *epd4in2.Device) {
+
+	black := color.RGBA{1, 1, 1, 255}
+	// white := color.RGBA{0, 0, 0, 255}
+
+	tinyfont.WriteLineRotated(display, &freemono.Bold9pt7b, 15, 20, "Hello, today is the first day of the rest of your life!", black, tinyfont.NO_ROTATION)
+	println("Waiting for 3 seconds")
+	time.Sleep(3 * time.Second)
+
+	// showRect(0, 22, 52, 20, black)
+	// showRect(52, 22, 52, 20, white)
+
+	// display.Display()
+	// display.WaitUntilIdle()
+
+	// println("Waiting for 5 seconds")
+	// time.Sleep(5 * time.Second)
+
+	// tinyfont.WriteLineRotated(&display, &freemono.Bold9pt7b, 85, 26, "World!", white, tinyfont.ROTATION_180)
+	// tinyfont.WriteLineRotated(&display, &freemono.Bold9pt7b, 55, 60, "@tinyGolang", black, tinyfont.ROTATION_90)
+	// tinyfont.WriteLineColorsRotated(&display, &freemono.Bold9pt7b, 45, 180, "tinyfont", []color.RGBA{white, black}, tinyfont.ROTATION_270)
+
+	println("Display()")
+	display.Display()
+	println("WaitUntilIdle()")
+	display.WaitUntilIdle()
+
+	println("Waiting for 3 seconds")
+	time.Sleep(3 * time.Second)
 }
