76e8991d0976d19ae6b4b4f58711cda4e6fdbb08
Tony Gilkerson <tgilkerson@miletwo.us>
2023-09-06 21:47:01 -0400

    restore epaber to spi0

--- proj29-epaper/main.go
+++ proj29-epaper/main.go
@@ -8,6 +8,7 @@ import (
 
 	"tinygo.org/x/drivers/waveshare-epd/epd4in2"
 	"tinygo.org/x/tinyfont"
+
 	// "tinygo.org/x/tinyfont/freemono"
 	"tinygo.org/x/tinyfont/gophers"
 )
@@ -22,14 +23,12 @@ func main() {
 	var dc machine.Pin = machine.GP11
 	var rst machine.Pin = machine.GP12
 	var busy machine.Pin = machine.GP13
-	var cs machine.Pin = machine.GP10
+	var cs machine.Pin = machine.GP17
 	var clk machine.Pin = machine.GP18 // machine.SPI0_SCK_PIN
 	var din machine.Pin = machine.GP19 // machine.SPI0_SDO_PIN
 
-	
 	time.Sleep(2 * time.Second)
 	println("Starting...")
-
 
 	println("Configure SPI...")
 	machine.SPI0.Configure(machine.SPIConfig{
@@ -47,10 +46,11 @@ func main() {
 
 	// clearDisplay(&display)
 	// displayCheckerBoard(&display)
-	
-	clearDisplay(&display)
-	fontExamples(&display)
 
+	println("clearDisplay()")
+	clearDisplay(&display)
+	println("fontExamples()")
+	fontExamples(&display)
 
 	println("Waiting for 5 seconds")
 	time.Sleep(5 * time.Second)
@@ -66,7 +66,7 @@ func showRect(x int16, y int16, w int16, h int16, c color.RGBA) {
 	}
 }
 
-func clearDisplay(display *epd4in2.Device){
+func clearDisplay(display *epd4in2.Device) {
 
 	println("ClearBuffer")
 	display.ClearBuffer()
@@ -78,7 +78,7 @@ func clearDisplay(display *epd4in2.Device){
 	time.Sleep(3 * time.Second)
 }
 
-func displayCheckerBoard(display *epd4in2.Device){
+func displayCheckerBoard(display *epd4in2.Device) {
 	black := color.RGBA{1, 1, 1, 255}
 
 	println("Prep checkered board")
@@ -93,7 +93,7 @@ func displayCheckerBoard(display *epd4in2.Device){
 
 	println("Waiting for 3 seconds")
 	time.Sleep(3 * time.Second)
-	
+
 	println("Display()")
 	display.Display()
 	println("WaitUntilIdle()")
@@ -109,7 +109,7 @@ func fontExamples(display *epd4in2.Device) {
 	// white := color.RGBA{0, 0, 0, 255}
 
 	//tinyfont.WriteLineRotated(display, &freemono.Bold9pt7b, 15, 20, "a", black, tinyfont.NO_ROTATION)
-	
+
 	time.Sleep(3 * time.Second)
 
 	// showRect(0, 22, 52, 20, black)
@@ -123,11 +123,11 @@ func fontExamples(display *epd4in2.Device) {
 
 	// tinyfont.WriteLineRotated(&display, &freemono.Bold9pt7b, 85, 26, "World!", white, tinyfont.ROTATION_180)
 	// tinyfont.WriteLineRotated(&display, &freemono.Bold9pt7b, 55, 60, "@tinyGolang", black, tinyfont.ROTATION_90)
-	tinyfont.WriteLineRotated(display, &gophers.Regular58pt, 40, 50,  "ABCDEFG\nHIJKLMN\nOPQRSTU\nVWXYZ\nabc", black, tinyfont.NO_ROTATION)
+	tinyfont.WriteLineRotated(display, &gophers.Regular58pt, 40, 50, "ABCDEFG\nHIJKLMN\nOPQRSTU", black, tinyfont.NO_ROTATION)
+	// tinyfont.WriteLineRotated(display, &gophers.Regular58pt, 40, 50,  "ABCDEFG\nHIJKLMN\nOPQRSTU\nH", black, tinyfont.NO_ROTATION)
 	// tinyfont.WriteLineColorsRotated(&display, &freemono.Bold9pt7b, 45, 180, "tinyfont", []color.RGBA{white, black}, tinyfont.ROTATION_270)
-	
-	println("Waiting for 3 seconds")
 
+	println("Waiting for 3 seconds")
 
 	println("Display()")
 	display.Display()
